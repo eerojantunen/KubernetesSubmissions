33aab4ff9b5cb305156ea42d75fb22646de2ab96
eerojantunen <jant.eero@gmail.com>
2026-01-21 23:19:03 +0200

    2.5

--- log-output/log-getter/log-getter.py
+++ log-output/log-getter/log-getter.py
@@ -6,16 +6,25 @@ app = FastAPI()
 
 directory = os.path.join('shared','files')
 filePath = os.path.join(directory, 'log.txt')
+config = os.path.join('config')
 #filePathPong = os.path.join(directory, 'pingpong.txt')
-
 
 @app.get("/status")
 def status():
+
+    env_message = os.getenv("MESSAGE", "not found")        
     try:
         with open(filePath, "r") as f:
             content = f.read()
+        with open("/config/information.txt", "r") as f:
+            file_content = f.read().strip()
         pong = requests.get("http://ping-pong-svc:2346/pings").text
-        result = f"{content} Ping / Pongs: {pong}"
+        result = (
+                 f"file content: {file_content}\n"
+                 f"env variable: MESSAGE={env_message}\n"
+                 f"{content}\n"
+                 f"Ping / Pongs: {pong}"
+        )
         return result
     except Exception as e:
         return str(e) 
