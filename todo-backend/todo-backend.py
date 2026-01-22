915e5461763c557574f1bd5b7c784796fb203be0
eerojantunen <jant.eero@gmail.com>
2026-01-22 22:57:40 +0200

    2.8

--- todo-backend/todo-backend.py
+++ todo-backend/todo-backend.py
@@ -4,27 +4,62 @@ from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from datetime import datetime
 import requests
+from sqlalchemy import create_engine, text
 
 app = FastAPI()
 
 directory = os.path.join('shared','files')
 
 port = int(os.getenv("PORT", 8000))
+DB_URL = (os.getenv("DB_URL"))
 
 todo_list = ["Learn JacaScript", "Learn React", "Build a project"]
 redirect_url = os.getenv("REDIRECT_URL")
 
+engine = create_engine(DB_URL)
+
+def init_db():
+    with engine.connect() as conn:
+        exists = conn.execute(text("""
+            SELECT EXISTS (
+                SELECT FROM information_schema.tables 
+                WHERE table_schema = 'public' 
+                AND table_name = 'todos'
+            );
+        """)).scalar()
+        
+        if not exists:
+            conn.execute(text(
+                "CREATE TABLE IF NOT EXISTS todos (id SERIAL PRIMARY KEY, todo TEXT)"))
+            conn.commit()
+            for todo in todo_list:
+                conn.execute(
+                    text("INSERT INTO todos (todo) VALUES (:todo) ON CONFLICT DO NOTHING"),
+                    {"todo": todo})        
+                conn.commit()
+
+init_db()
+
 @app.get("/todos")
 async def get_todos():
-    global todo_list
-    return todo_list
+    with engine.connect() as conn:
+        result = conn.execute(text(
+            "SELECT todo FROM todos ORDER BY id ASC"
+        ))
+    todos_list = [row[0] for row in result]
+    return todos_list
 
 @app.post("/todos")
 async def post_todos(request: Request):
-    global todo_list
     data = await request.form()
     todo = data.get("todo")
-    todo_list.append(todo)
+    with engine.connect() as conn:
+        conn.execute(
+            text("INSERT INTO todos (todo) VALUES (:todo)"),
+            {"todo": todo}
+        )
+        conn.commit()
+
     return RedirectResponse(url=redirect_url, status_code=303)
 
 
