5a2c40aabec796d5c6e8aaacbdcaaa332dc6b841
eerojantunen <jant.eero@gmail.com>
2026-01-28 20:35:09 +0200

    4.5

--- project/todo-app/todo_app.py
+++ project/todo-app/todo_app.py
@@ -20,6 +20,7 @@ port = int(os.getenv("PORT", 8000))
 env_url = os.getenv("PICSUM_URL")
 todos_url = os.getenv("TODOS_URL")
 todos_health_url = os.getenv("TODOS_HEALTH_URL")
+todos_done_url = os.getenv("TODOS_DONE_URL")
 
 def download_image():
     response = requests.get(env_url)    
@@ -34,8 +35,20 @@ def get_todos():
     todo_list_html = ""
 
     for todo in todos_list:
-        todo_list_html += f"<li>{todo}</li>"
+        todo_list_html += (
+            f"<li>{todo['content']} "
+            f"<button onclick=\"markDone({todo['id']})\">Mark as done</button>"
+            f"</li>"
+        )
     return todo_list_html
+
+def get_done():
+    done_list = requests.get(todos_done_url).json()
+    done_list_html = ""
+
+    for done in done_list:
+        done_list_html += f"<li>{done}</li>"
+    return done_list_html
 
 @app.get("/healthz")
 def healthz():
@@ -47,9 +60,22 @@ def healthz():
         return Response(status_code=200)
     return Response(status_code=500)
 
-@app.get("/", response_class=HTMLResponse)
-async def root():
+@app.post("/markdone/{id}")
+def markdone(id):
+    try:
+        url = f"{todos_url}/{id}/done"
+        markdone_status = requests.put(url)
+        return Response(status_code=markdone_status.status_code)
+    except Exception:
+        return Response(status_code=500)
 
+
+
+
+
+
+@app.get("/", response_class=HTMLResponse)
+def root():
     if not os.path.exists(time_filePath):
         download_image()
 
@@ -61,19 +87,39 @@ async def root():
 
 
     return f"""
-    <html>
-        <body>
-            <h1>The project App</h1>
-            <img src="/static/image.jpg" style="max-width: 500px;">
-            <form action="/todos" method="POST">
-                <input type="text" name="todo" maxlength="140" required>
-                <button type="submit">Create todo</button>
-            </form>
-            {get_todos()}
-            <p>DevOps with Kubernetes 2025<p>
-        </body>
-    </html>
-    """
+        <html>
+            <body>
+                <script>
+                    async function markDone(todoId) {{
+                        const response = await fetch('/markdone/' + todoId, {{
+                            method: 'POST',
+                        }});
+                        if (response.ok) {{
+                            location.reload();
+                        }} else {{
+                            alert('Failed to update todo');
+                        }}
+                    }}
+                </script>
+                
+                <h1>The project App</h1>
+                <img src="/static/image.jpg" style="max-width: 500px;">
+                
+                <form action="/todos" method="POST">
+                    <input type="text" name="todo" maxlength="140" required>
+                    <button type="submit">Create todo</button>
+                </form>
+                
+                <h2>Todo</h2>
+                {get_todos()}
+                <h2>Done</h2>
+                {get_done()}
+                
+                <p>DevOps with Kubernetes 2025 University of Helsinki</p>
+            </body>
+        </html>
+        """
+
 
 print(f"Server started in port {port}")
 uvicorn.run(app, host="0.0.0.0", port=port)
