510aad8a411042ef0dc08fee4c77951bf4a030ac
eerojantunen <jant.eero@gmail.com>
2026-01-29 22:34:29 +0200

    test 4.9

--- project/broadcaster/broadcaster.py
+++ project/broadcaster/broadcaster.py
@@ -3,7 +3,7 @@ import asyncio
 import nats
 import requests
 
-DISCORD_URL = os.getenv("DISCORD_URL")
+DISCORD_URL = os.getenv("DISCORD_URL", "none")
 NATS_URL = os.getenv("NATS_URL")
 
 async def main():
@@ -11,9 +11,11 @@ async def main():
 
     async def callback(msg):
         data = msg.data.decode()
-        requests.post(DISCORD_URL, json={"content": data})
+        print(data)
+        if DISCORD_URL and DISCORD_URL != "none":
+            requests.post(DISCORD_URL, json={"content": data})
 
     await nc.subscribe("todos", cb=callback, queue="workers")
     await asyncio.Future()
-    
+
 asyncio.run(main())
